8cd86e0884498656f557c9b4031c237ff73e10b9
Ryan Abernathey <ryan.abernathey@gmail.com>
2023-12-02 23:15:09 -0500

    remove ndarray dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,75 @@
+use numpy::PyArrayDyn;
 use pco::standalone::{auto_compress, simple_decompress_into};
 use pco::DEFAULT_COMPRESSION_LEVEL;
-use numpy::{PyArrayDyn, PyReadonlyArrayDyn};
-use pyo3::prelude::{pymodule, FromPyObject, PyModule, PyResult, Python, PyObject};
+use pyo3::exceptions::PyRuntimeError;
+use pyo3::prelude::{pymodule, FromPyObject, PyModule, PyObject, PyResult, Python};
 use pyo3::types::PyBytes;
+
+// The Numpy crate recommends using this type of enum to write functions that accept different Numpy dtypes
+// https://github.com/PyO3/rust-numpy/blob/32740b33ec55ef0b7ebec726288665837722841d/examples/simple/src/lib.rs#L113
+// Subsequently, we have to do a lot of repetitive `match` statements to handle the different dtypes.
+// There has to be a better way the involves less repetition, but I don't know enough Rust to figure it out.
+#[derive(FromPyObject)]
+enum ArrayDynFloat<'py> {
+    F32(&'py PyArrayDyn<f32>),
+    F64(&'py PyArrayDyn<f64>),
+}
 
 #[pymodule]
 fn pcodec(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-
-    // This is a pure function (no mutations of incoming data).
-    // You can see this as the python array in the function arguments is readonly.
-    // The object we return will need ot have the same lifetime as the Python.
-    // Python will handle the objects deallocation.
-    // We are having the Python as input with a lifetime parameter.
-    // Basically, none of the data that comes from Python can survive
-    // longer than Python itself. Therefore, if Python is dropped, so must our Rust Python-dependent variables.
     #[pyfn(m)]
-    fn compress<'py>(py: Python<'py>, x: PyReadonlyArrayDyn<f64>) -> PyObject {
-        // Here we have a numpy array of dynamic size. But we could restrict the
-        // function to only take arrays of certain size
-        // e.g. We could say PyReadonlyArray3 and only take 3 dim arrays.
-        // These functions will also do type checking so a
-        // numpy array of type np.float32 will not be accepted and will
-        // yield an Exception in Python as expected
-        let array = x.as_array();
-        let slice = array.as_slice().unwrap();
-        let compressed: Vec<u8> = auto_compress(&slice, DEFAULT_COMPRESSION_LEVEL);
-        PyBytes::new(py, &compressed).into()
+    fn compress<'py>(py: Python<'py>, x: ArrayDynFloat<'py>) -> PyObject {
+        match x {
+            ArrayDynFloat::F32(x) => {
+                let ro = x.readonly();
+                let array = ro.as_array();
+                let slice = array.as_slice().unwrap();
+                let compressed: Vec<u8> = auto_compress(&slice, DEFAULT_COMPRESSION_LEVEL);
+                PyBytes::new(py, &compressed).into()
+            }
+            ArrayDynFloat::F64(x) => {
+                let ro = x.readonly();
+                let array = ro.as_array();
+                let slice = array.as_slice().unwrap();
+                let compressed: Vec<u8> = auto_compress(&slice, DEFAULT_COMPRESSION_LEVEL);
+                PyBytes::new(py, &compressed).into()
+            }
+        }
     }
 
     #[pyfn(m)]
-    fn decompress<'py>(compressed: &PyBytes, out: &PyArrayDyn<f64>) {
-        let src: &[u8] = compressed.extract().unwrap();
-        let mut out_rw = out.readwrite();
-        let dst = out_rw.as_slice_mut().expect("failed to get mutable slice");
-        let _result = simple_decompress_into::<f64>(src, dst).expect("failed to decompress");
+    fn decompress<'py>(compressed: &PyBytes, out: ArrayDynFloat<'py>) -> PyResult<()> {
+        match out {
+            ArrayDynFloat::F32(out) => {
+                let mut out_rw = out.readwrite();
+                let dst = out_rw.as_slice_mut().expect("failed to get mutable slice");
+                let src: &[u8] = compressed.extract().unwrap();
+                let progress =
+                    simple_decompress_into::<f32>(src, dst).expect("failed to decompress");
+                if progress.finished != true {
+                    Err(PyRuntimeError::new_err(
+                        "decompression didn't finish. Buffer too small?",
+                    ))
+                } else {
+                    Ok(())
+                }
+            }
+            ArrayDynFloat::F64(out) => {
+                let mut out_rw = out.readwrite();
+                let dst = out_rw.as_slice_mut().expect("failed to get mutable slice");
+                let src: &[u8] = compressed.extract().unwrap();
+                let progress =
+                    simple_decompress_into::<f64>(src, dst).expect("failed to decompress");
+                if progress.finished != true {
+                    Err(PyRuntimeError::new_err(
+                        "decompression didn't finish. Buffer too small?",
+                    ))
+                } else {
+                    Ok(())
+                }
+            }
+        }
     }
-
 
     Ok(())
 }
--- test/test_pcodec.py
+++ test/test_pcodec.py
@@ -2,16 +2,23 @@ import numpy as np
 from pcodec import compress, decompress
 import pytest
 
+rng = np.random.default_rng(12345)
+
 @pytest.mark.parametrize(
-    "data",
+    "shape",
     [
-        np.arange(0, 100, 1000)
-    ]
+        (100,),
+        (100, 100),
+        (10, 10, 100),
+        (2, 10, 10, 50),
+    ],
 )
 @pytest.mark.parametrize("dtype", ['f4', 'f8'])
-def test_round_trip(data, dtype):
-    data = data.astype(dtype)
+def test_round_trip(shape, dtype):
+    data = rng.random(shape, dtype)
     compressed = compress(data)
     out = np.empty_like(data)
     decompress(compressed, out)
     np.testing.assert_array_equal(data, out)
+    with pytest.raises(RuntimeError, match="too small"):
+        decompress(compressed, np.empty_like(out, dtype=dtype)[:-1])
